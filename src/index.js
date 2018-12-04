752827ae9e5078fe660f2fa69497eb596976bc97
Tim Work Laptop <tmstani23@gmail.com>
2018-12-04 15:30:31 -0600

    Update 3
    Fixed Apollo client to use uri instead of url and the correct env variable notation.  Added two get repositories queries.  The first gets the first 5 repos and the second includes pagination and takes it's cursor location from the endCursor variable in the first query.  The second query also displayes the first 10 repos ordered by stargazer count.  Added an add star and remove star mutation.  Added mutation clients that pass the mutation string and variables and make the request to the Github API.

--- src/index.js
+++ src/index.js
@@ -4,30 +4,116 @@ import 'dotenv/config';
 
 
 const client = new ApolloClient({
-  url: 'https://api.github.com/graphql',
+  uri: 'https://api.github.com/graphql',
   request: operation => {
       operation.setContext({
       headers: {
-        authorization: `Bearer YOUR_GITHUB_PERSONAL_ACCESS_TOKEN`,
+        authorization: `Bearer ${process.env.YOUR_GITHUB_PERSONAL_ACCESS_TOKEN}`,
       },
     });
   },
 });
 
-const GET_ORGANIZATION = gql`
-  {
-    organization(login: "the-road-to-learn-react") {
+const GET_REPOSITORIES_OF_ORGANIZATION = gql`
+  query first($organization: String!) {
+    organization(login: $organization) {
       name
       url
+      repositories(first: 5 orderBy: {direction: DESC, field: STARGAZERS}) {
+        edges {
+          node {
+            name
+            url
+          }  
+        }
+        pageInfo {
+          endCursor
+          hasNextPage
+        }
+      }
     }
   }
 `;
+const GET_REPOSITORIES_OF_ORGANIZATION_PAGINATED = gql`
+query second($organization: String! $cursor: String) {
+  organization(login: $organization) {
+    name
+    url
+    repositories(first: 10 after:$cursor orderBy: {direction: DESC, field: STARGAZERS}) {
+      edges {
+        node {
+          name
+          url
+        }  
+      }
+      pageInfo {
+      	endCursor
+        hasNextPage
+      }
+    }
+  }
+}
+`;
+const ADD_STAR = gql`
+  mutation AddStar($repositoryId: ID!) {
+    addStar(input: {starrableId: $repositoryId }) {
+      starrable {
+        id
+        viewerHasStarred
+      }
+    }
+  }
+`
+const REMOVE_STAR = gql`
+  mutation AddStar($repositoryId: ID!) {
+    removeStar(input: {starrableId: $repositoryId }) {
+      starrable {
+        id
+        viewerHasStarred
+      }
+    }
+  }
+`
 
-console.log(client
-  .query({
-    query: GET_ORGANIZATION,
+let cursor = "";
+// client
+//   .query({
+//     query: GET_REPOSITORIES_OF_ORGANIZATION,
+//     variables: {
+//       organization: 'the-road-to-learn-react'
+//     },
+//   })
+//   .then((result) => cursor = result.data.organization.repositories.pageInfo.endCursor)
+
+// client  
+//   .query({
+//     query: GET_REPOSITORIES_OF_ORGANIZATION_PAGINATED,
+//     variables: {
+//       organization: 'the-road-to-learn-react',
+//       cursor: "NQ"
+//     },
+//   })
+//   .then((result) => console.log(JSON.stringify(result.data.organization.repositories, null, 4)))
+  
+//Add Star Mutation request:
+client
+  .mutate({
+    mutation: ADD_STAR,
+    variables: {
+      repositoryId: 'MDEwOlJlcG9zaXRvcnk2MzM1MjkwNw==',
+    }
   })
-  .then(console.log)
-)
+  .then(result => console.log(JSON.stringify(result, null, 6)));
+//Remove Star Mutation
+  client
+  .mutate({
+    mutation: REMOVE_STAR,
+    variables: {
+      repositoryId: 'MDEwOlJlcG9zaXRvcnk2MzM1MjkwNw==',
+    }
+  })
+  .then(result => console.log(JSON.stringify(result, null, 6)));
+ 
+
 
 
